6d59d2ae4627f685c9022ad98fcfe8b4214e0f6a
mujeeb909 <65164185+mujeeb909@users.noreply.github.com>
2021-12-21 20:19:43 +0500

    make it better

--- website/urls.py
+++ website/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -25,6 +26,8 @@ urlpatterns = [
     path('', include('store.urls')),
     path('', include('accounts.urls')),
     path('', include('order.urls')),
-    path('', include('review.urls'))
+    path('', include('review.urls')),
+
+    re_path(r'^media/(?<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT})
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
